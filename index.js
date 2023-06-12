6aab5ec3487e3b51fd0900726b6d221cb680ac3e
Lim-Junha <nomuhyeon34@gmail.com>
2023-06-12 20:58:21 +0900

    777

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require('express');
-const serverless = require('serverless-http');
 const path = require('path');
 const app = express();
 const router = express.Router();
 
-app.set('port', process.env.PORT || 3000);
-app.set('view engine','html')
-
 app.use(express.static('public'));
 
+app.set('port',3000)
+
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
+
 app.use('/', router);
 
-app.use((req,res,next) => {
-    res.status(404).send('Not Found');
-})
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
+});
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
-
-module.exports.handler = serverless(app)
