151341c8af4776c258eac5f18212ca10146d60df
Lim-Junha <nomuhyeon34@gmail.com>
2023-06-12 20:33:32 +0900

    444

---
+++ functions/api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const path = require('path');
+const app = express();
+const router = express.Router();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine','html')
+
+app.use(express.static('public'));
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'/index.html'));
+});
+app.use('/', router);
+
+app.use((req,res,next) => {
+    res.status(404).send('Not Found');
+})
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
+
+app.use('/.netlify/functions/api', router)
+module.exports.handler = serverless(app)
