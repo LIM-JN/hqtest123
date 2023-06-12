53f2053485dd804c4d64f348be3c5829aa1f9005
Lim-Junha <nomuhyeon34@gmail.com>
2023-06-12 14:57:04 +0900

    1-1

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express()
+
+app.set('port',process.env.PORT || 3000)
+
+app.get('/',(res,req) => {
+    app.render('index.html')
+})
+
+app.listen(3000,() => {
+    console.log(app.get('port'),'번 포트에서 대기중')
+})
