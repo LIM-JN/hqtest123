555c186172ba728a5a50ec0c32b2bfdc96d10c56
Lim-Junha <nomuhyeon34@gmail.com>
2023-06-12 18:13:05 +0900

    d

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.set('port',process.env.PORT || 3000)
 
 app.get('/',(res,req) => {
-    app.render('index.html')
+    app.render('./build/index.html')
 })
 
 app.listen(3000,() => {
